5073af24694c3f48a109b8861ebd6f8bda2487d8
dance2die <sungmkim80@gmail.com>
2021-06-16 20:17:59 -0400

    fixed prewview list margin for left arrow

--- components/Preview/SessionPreviewList.js
+++ components/Preview/SessionPreviewList.js
@@ -6,68 +6,55 @@ import SessionPreview from '../Preview/SessionPreview';
 import RightArrow from '../RightArrow';
 import LeftArrow from '../LeftArrow';
 
-const isHorizontalScrollReachedEnd = e =>
-  e.target.scrollWidth - e.target.clientWidth - e.target.scrollLeft === 0;
+const isHorizontalScrollReachedEnd = (e) => e.target.scrollWidth - e.target.clientWidth - e.target.scrollLeft === 0;
 
 function SessionPreviewList({ previewData, backgroundGradient }) {
-  const [scrollLeft, setScrollLeft] = useState(0);
-  const [isEndReached, setIsEndReached] = useState(false);
-  const scrollBarRef = useRef(null);
+	const [ scrollLeft, setScrollLeft ] = useState(0);
+	const [ isEndReached, setIsEndReached ] = useState(false);
+	const scrollBarRef = useRef(null);
 
-  const updateScrollLeft = e => {
-    setScrollLeft(e.target.scrollLeft);
-    setIsEndReached(isHorizontalScrollReachedEnd(e));
-  };
+	const updateScrollLeft = (e) => {
+		setScrollLeft(e.target.scrollLeft);
+		setIsEndReached(isHorizontalScrollReachedEnd(e));
+	};
 
-  const scrollByOffset = 300;
-  const scrollToLeft = () => {
-    scrollBarRef.current.scrollBy({
-      left: -scrollByOffset,
-      behavior: 'smooth'
-    });
-  };
-  const scrollToRight = () => {
-    scrollBarRef.current.scrollBy({
-      left: scrollByOffset,
-      behavior: 'smooth'
-    });
-  };
+	const scrollByOffset = 300;
+	const scrollToLeft = () => {
+		scrollBarRef.current.scrollBy({
+			left: -scrollByOffset,
+			behavior: 'smooth'
+		});
+	};
+	const scrollToRight = () => {
+		scrollBarRef.current.scrollBy({
+			left: scrollByOffset,
+			behavior: 'smooth'
+		});
+	};
 
-  return (
-    <div className="w-full h-52 flex items-center">
-      <span
-        className={cx(
-          { visible: scrollLeft > 0 },
-          { invisible: scrollLeft === 0 }
-        )}
-        onClick={scrollToLeft}
-      >
-        <LeftArrow />
-      </span>
+	return (
+		<div className="w-full h-52 flex items-center">
+			<span
+				className={cx('-ml-4 sm:-ml-5', { visible: scrollLeft > 0 }, { invisible: scrollLeft === 0 })}
+				onClick={scrollToLeft}
+			>
+				<LeftArrow />
+			</span>
 
-      <div
-        className={cx(
-          'flex justify-between items-center gap-x-3 px-2 h-full w-full overflow-x-hidden'
-        )}
-        onScroll={updateScrollLeft}
-        ref={scrollBarRef}
-      >
-        {previewData.map(data => (
-          <SessionPreview
-            key={data.title}
-            data={data}
-            backgroundGradient={backgroundGradient}
-          />
-        ))}
-      </div>
-      <span
-        className={cx({ visible: !isEndReached }, { invisible: isEndReached })}
-        onClick={scrollToRight}
-      >
-        <RightArrow />
-      </span>
-    </div>
-  );
+			<div
+				className={cx('flex justify-between items-center gap-x-3  h-full w-full overflow-x-hidden')}
+				onScroll={updateScrollLeft}
+				ref={scrollBarRef}
+			>
+				{previewData.map((data) => (
+					<SessionPreview key={data.title} data={data} backgroundGradient={backgroundGradient} />
+				))}
+			</div>
+			<span className={cx({ visible: !isEndReached }, { invisible: isEndReached })} onClick={scrollToRight}>
+				<RightArrow />
+			</span>
+		</div>
+	);
 }
 
 export default SessionPreviewList;
